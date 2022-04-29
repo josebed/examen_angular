342746419ce46b5630f455120ecc136083e2f520
josebed <je.bedoya@uniandes.edu.co>
2022-04-29 11:27:18 -0500

    Showing count by branch

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -10,13 +10,39 @@ import { VehicleService } from '../vehicle.service';
 export class VehicleListComponent implements OnInit {
 
   vehicles: Array<Vehicle> = [];
+  vehicleBranches: Array<[string,number]> =[];
 
   constructor(private vehicleservice: VehicleService) { }
 
   getVehicles(): void{
     this.vehicleservice.getVehicles().subscribe((vehicles) =>{
       this.vehicles=vehicles;
+      console.log(vehicles.length);
+      this.calculateQtyBranch();
     });
+
+  }
+  calculateQtyBranch(): void{
+    let index: number =0;
+    let branchIndex: number = -1
+    let vehicleBranchQty: Array<number> =[];
+    let tempVehicleBranches: Array<string> = [];
+
+    for (index=0; index < this.vehicles.length; index++){
+      branchIndex = tempVehicleBranches.indexOf(this.vehicles[index].marca);
+
+      if(branchIndex == -1){
+        tempVehicleBranches.push(this.vehicles[index].marca);
+        vehicleBranchQty.push(1);
+      }
+      else{
+        vehicleBranchQty[branchIndex] ++;
+      }
+
+    }
+    for (index=0; index <tempVehicleBranches.length; index++){
+      this.vehicleBranches.push([tempVehicleBranches[index],vehicleBranchQty[index]]);
+    }
   }
 
   ngOnInit() {
